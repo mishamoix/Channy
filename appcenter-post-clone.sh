b53201ac2e92d0e3941e3bb53b37142a42ecb641
mishamoix <mishamoix@gmail.com>
2018-12-10 22:05:05 +0300

    fixed script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-cd Chan/Resources/
-curl -o GoogleService-Info.plist -X GET \
+curl -o Chan/Resources/GoogleService-Info.plist -X GET \
   '$FIREBASE_URL' \
   -H 'Accept-Encoding: gzip;q=1.0, compress;q=0.5' \
   -H 'Accept-Language: en;q=1.0, ru-RU;q=0.9' \
@@ -9,3 +8,4 @@ curl -o GoogleService-Info.plist -X GET \
 
 echo $FIREBASE_URL
 echo "Downloaded GoogleService-Info.plist"
+ls -l Chan/Resources/
