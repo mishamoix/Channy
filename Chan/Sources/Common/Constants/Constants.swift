1ec63df87406e7c7d3d970ba28dadb32d0bbb349
mishamoix <mishamoix@gmail.com>
2018-12-11 11:23:40 +0300

    fixed blur radius

--- Chan/Sources/Common/Constants/Constants.swift
+++ Chan/Sources/Common/Constants/Constants.swift
@@ -37,5 +37,5 @@ let DefaultDismissTime: TimeInterval = 2.0
 let SmallDismissTime: TimeInterval = 1.0
 
 
-let BlurRadiusPreview: CGFloat = 0.05
-let BlurRadiusOriginal: CGFloat = 0.01
+let BlurRadiusPreview: CGFloat = 0.07
+let BlurRadiusOriginal: CGFloat = 0.02
